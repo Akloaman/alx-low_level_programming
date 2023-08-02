05c542928aaf736aeb8762698859bd611519922c
Akloaman <akloaman4042@gmail.com>
2023-08-02 12:13:04 -0700

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -9,4 +10,4 @@ int is_prime_number(int n);
 int _pow_recursion(int x, int y);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-#endifx
+#endif
